acb7874bd3adb33bb6636110384e90bb3aeb79f3
Roman Belik <dabonka@yandex.ru>
2020-10-04 15:46:57 +0300

    add routing and components

---
+++ src/components/result/result.jsx
@@ -0,0 +1,19 @@
+import {React, Fragment} from "react";
+
+const Result = () => {
+
+  return (
+    <Fragment>
+      <section className="result">
+        <div className="result__logo">
+          <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
+        </div>
+        <h2 className="result__title">Вы настоящий меломан!</h2>
+        <p className="result__total">Вы ответили правильно на 6 вопросов и совершили 2 ошибки</p>
+        <button className="replay" type="button">Сыграть ещё раз</button>
+      </section>
+    </Fragment>
+  );
+};
+
+export default Result;
